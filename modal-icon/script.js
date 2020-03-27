60ee2dc91c2170f43a5ad0560f52534446339f54
aflapoid <aflapoid.fenix@gmail.com>
2020-03-27 22:18:27 +0200

    Fix bugs

--- modal-icon/script.js
+++ modal-icon/script.js
@@ -8,7 +8,7 @@ btn.onclick = function () {
 span.onclick = function () {
   modal.style.display = "none"
 }
-window.onclick - function () {
+window.onclick = function () {
   if (event.target == modal) {
     modal.style.display = "none";
   }
